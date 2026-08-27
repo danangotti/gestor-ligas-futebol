fd9a691a27ea2887b1ad3c830c69be87f6e63a90
danangotti <dan.angotti@sga.pucminas.br>
2026-08-27 16:34:43 -0300

    Adiciona script SQL de criacao do banco de dados PostgreSQL

---
+++ script_criacao_banco.sql
@@ -0,0 +1,24 @@
+﻿-- 1. Criação das Tabelas
+CREATE TABLE liga (
+    id_liga SERIAL PRIMARY KEY,
+    nome_liga VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE time (
+    id_time SERIAL PRIMARY KEY,
+    nome_time VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    id_liga INT NOT NULL,
+    CONSTRAINT fk_time_liga FOREIGN KEY (id_liga) REFERENCES liga(id_liga)
+);
+
+CREATE TABLE jogador (
+    id_jogador SERIAL PRIMARY KEY,
+    nome_jogador VARCHAR(100) NOT NULL,
+    posicao VARCHAR(30) NOT NULL,
+    numero_camisa INT,
+    gols_jogador INT DEFAULT 0,
+    ass_jogador INT DEFAULT 0,
+    id_time INT NOT NULL,
+    CONSTRAINT fk_jogador_time FOREIGN KEY (id_time) REFERENCES time(id_time)
+);
